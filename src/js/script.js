2b16549dc17b95ea48ebd6ca0d654adb2588ec1c
Syr1tsa <33559034+Syr1tsa@users.noreply.github.com>
2019-04-10 17:15:30 +0300

    Add modals with Video Player
    Added autoslice title 
    Fixed bug with night mode.
    Added open modal and close modal when user click outside modal area and click 'esc'
    Added in modal video player

--- src/js/script.js
+++ src/js/script.js
@@ -107,5 +107,98 @@ function switchMode() {
         setTimeout(() => {
             card.classList.remove('videos__item-active');
         }, 10);
+        bindNewModal(card);
     }
+    
+    if (night === true) {
+        document.querySelectorAll('.videos__item-descr').forEach(item => {
+          item.style.color = '#fff';
+          });
+        
+          document.querySelectorAll('.videos__item-views').forEach(item => {
+            item.style.color = '#fff';
+            });
+      }
+    sliceTitle('.videos__item-descr', 100);
  });
+
+function sliceTitle(selector,count) { 
+    document.querySelectorAll(selector).forEach(item => {
+        item.textContent.trim();
+
+        if (item.textContent.length < count) {
+            return;
+        } else {
+            const str = item.textContent.slice(0, count+1) + "...";
+            item.textContent = str;
+        }
+    });
+}
+
+sliceTitle('.videos__item-descr', 80);
+
+function openModal() { 
+    modal.style.display = 'block';
+ }
+
+function closeModal() { 
+    modal.style.display = 'none';
+ }
+
+function bindModal(cards) { 
+    cards.forEach( item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            const id = item.getAttribute('data-url');
+            loadVideo(id);
+            openModal();
+        });
+    });
+ }
+
+bindModal(videos);
+
+function bindNewModal(cards) { 
+    cards.addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = cards.getAttribute('data-url');
+        loadVideo(id);
+        openModal();
+    });
+ }
+
+modal.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__body')){
+        closeModal();
+        player.stopVideo();
+    }
+});
+ 
+document.onkeydown = function (e) { // Для закрытия модальных окон на esc
+    if (e.keyCode == 27) {
+        closeModal();
+        player.stopVideo();
+    }
+ }
+
+function createVideo(param) { 
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    setTimeout( () => {
+        player = new YT.Player('frame', {
+            height: '100%',
+            width: '100%',
+            videoId: 'M7lc1UVf-VE',
+          });
+    }, 300);
+}
+
+createVideo();
+
+function loadVideo(id) { 
+    player.loadVideoById({'videoId' : `${id}`});
+ }
